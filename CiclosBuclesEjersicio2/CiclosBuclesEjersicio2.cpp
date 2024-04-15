3f85b2ff4085df79e394b28a42562439ec850e25
Junior11995 <junior22ascinsero@gmail.com>
2024-04-15 19:34:19 -0400

    Agregar archivos de proyecto.

---
+++ CiclosBuclesEjersicio2/CiclosBuclesEjersicio2.cpp
@@ -0,0 +1,28 @@
+/*
+* 22. Programación en C++ || Ciclos o Bucles || Ejercicio Tabla de multiplicar de un número
+* 
+Ejercicio 2: Realice un programa que lea de la entrada estándar números hasta que se introduzca un cero. 
+En ese momento el programa debe terminar y mostrar en la salida estándar el número de valores mayores que cero leídos.
+*/
+
+#include <iostream>
+#include <stdlib.h>
+#include <conio.h>
+
+
+using namespace std;
+
+int main() {
+
+	int numero,contador =0;
+
+	cout << "introdusca un numero: "; cin >> numero;
+
+	
+
+
+	cout << "\n\n";
+	system("pause");
+	return 0;
+}
+
